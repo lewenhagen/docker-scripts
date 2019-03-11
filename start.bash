68798bd654e75a13bf64ad2273c722be6f12bf5e
Kenneth Lewenhagen <lewenhagen@live.com>
2019-03-11 14:46:48 +0100

    one and only

---
+++ start.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# imagesorter-v2
+#
+# A script to initialize imagesorter-v2 Docker image
+
+mkdir Result Unsorted
+
+if [[ -z "$(ls -A Unsorted)" ]]; then
+    echo "No images in the Unsorted folder!"
+else
+    docker run -it -v $(pwd)/Result:/Result -v $(pwd)/Unsorted:/Unsorted imagesorter
+fi 
